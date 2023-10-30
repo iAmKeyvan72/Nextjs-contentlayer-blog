ddc91760ab5dd5d59e20c00d5f3b7405d6780dad
Keyvan Mahmoudi <keyvanmahmoudi72@gmail.com>
2023-10-30 18:51:12 +0100

    Finish navigation

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,41 @@
+import Link from 'next/link';
 import Logo from './Logo';
+import {
+  DribbbleIcon,
+  GithubIcon,
+  LinkedinIcon,
+  SunIcon,
+  TwitterIcon,
+} from '../icons';
 
 const Header = () => {
   return (
     <header className="w-full p-4 px-10 flex items-center justify-between">
       <Logo />
+
+      <nav className="w-max py-3 px-8 border border-solid border-dark rounded-full font-medium capitalize flex items-center gap-4 fixed top-6 right-1/2 translate-x-1/2 bg-light/80 backdrop-blur-sm">
+        <Link href="/">Home</Link>
+        <Link href="/about">About</Link>
+        <Link href="/contact">Contact</Link>
+        <button>
+          <SunIcon />
+        </button>
+      </nav>
+
+      <div className="flex gap-4">
+        <a href="http://example.com" className="inline-block w-6 h-6">
+          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="inline-block w-6 h-6">
+          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="inline-block w-6 h-6">
+          <GithubIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+        <a href="http://example.com" className="inline-block w-6 h-6">
+          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
+        </a>
+      </div>
     </header>
   );
 };
